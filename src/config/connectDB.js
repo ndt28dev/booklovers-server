cda6a8ad1e2e4a2c5b9adb5e94dedf81afa44b3e
Nguyễn Duy Thuần <ndt28dev@gmail.com>
2026-03-08 12:14:55 +0700

    first commit

---
+++ src/config/connectDB.js
@@ -0,0 +1,25 @@
+// import mysql from "mysql2/promise";
+// require("dotenv").config();
+
+// const pool = mysql.createPool({
+//   host: process.env.MYSQLHOST,
+//   user: process.env.MYSQLUSER,
+//   password: process.env.MYSQLPASSWORD,
+//   database: process.env.MYSQLDATABASE,
+//   port: process.env.MYSQLPORT,
+// });
+
+// export default pool;
+
+import mysql from "mysql2/promise";
+require("dotenv").config();
+
+const pool = mysql.createPool({
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: Number(process.env.MYSQLPORT), // nhớ ép kiểu nếu cần
+});
+
+export default pool;
